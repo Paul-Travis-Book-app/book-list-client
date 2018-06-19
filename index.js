cce9e783f52444c50dbd50c32270750f307c4364
Paul Suarez <paulsblogging.com@gmail.com>
2018-06-19 11:59:18 -0700

    mostly done with indexJS about to finish handlebars real quick

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+let bookView = bookView || {};
+
+
+function Book(bookObj) {
+    Object.keys(bookObj).forEach(key => this[key] = bookObj [key]);
+
+}
+
+Book.prototype.toHtml = function () {
+    var template = Handlebars.compile($('#book-list-template').text());
+    return template(this);
+}
+
+Book.all = [];
+
+Book.loadAll = rows => {
+    rows.sort(function (a, b) {
+
+        let authorA = a.title.toUpperCase();
+        let authorB = b.title.toUpperCase();
+        if (authorA < authorB) {
+          return -1;
+        } if (authorA > authorB) {
+          return 1;
+        }
+        return 0;
+      });
+
+    Book.all = rows.map((info) => new Book(info));
+}
+
+Book.fetchAll = callback => {
+    $.get('/api/v1/books')
+        .then(results => {
+            Book.loadAll(results);
+            callback();
+        });
+}
+
+bookView.initIndexPage = () => {
+    $('.container').hide();
+    $('.book-view').show();
+    bookView.Book.all.forEach(a => $('#book-list').append(a.toHtml()));
+}
+
+
+$(document).ready(function () {
+    Book.fetchAll(bookView.initIndexPage);
+
+});
