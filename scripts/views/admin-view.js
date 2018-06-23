a09aad65132d0cd9c8ede3fb26bcc85961dd431c
Paul Suarez <paulsblogging.com@gmail.com>
2018-06-22 17:39:50 -0700

    starting project week cause its friday

--- scripts/views/admin-view.js
+++ scripts/views/admin-view.js
@@ -1,30 +1,25 @@
 'use strict';
 
 var adminView = {}
-
-
-
-    (function (module) {
-        
-    module.initPage = () => {
+    
+//(function (module) {
+    adminView.initPage = () => {
         $('.container').hide();
-        $('.admin-view').show();
+        $('#newbookform').show();
         $('#newbookform').empty();
     }
 
-    module.createBook = event => {
-        event.preventDefault()
-        let book = new Book({
-            title: $('#title').val(),
-            author: $('#author').val(),
-            isbn: $('#isbn').val(),
-            image_url: $('#image-url').val(),
-            description: $('#description').val(),
-        })
+        adminView.createBook = event => {
+            event.preventDefault()
+            let book = new Book({
+                title: $('#title').val(),
+                author: $('#author').val(),
+                isbn: $('#isbn').val(),
+                image_url: $('#image-url').val(),
+                description: $('#description').val(),
+            })
+        }
 
-    //  We need to call the add-book post route here
-    }
-        
+        book.postOne();
 
-})(adminView)
-    
+//})(adminView)
